2cc2452e8c50279e33ced25708da5f9865af8646
Or Zellinger <or.zellinger@gmail.com>
2025-07-24 10:04:34 +0300

    Fixed

--- Web/reqs.py
+++ Web/reqs.py
@@ -1,8 +1,7 @@
 import requests
 
-API_LINK = 'http://127.0.0.1:8000/1,1,1,1,1,-1,0,1,-1,1,1,-1,1,0,-1,-1,1,1,0,1,1,1,1,-1,-1,0,-1,1,1,1'
+API_LINK = 'http://127.0.0.1:8000/check?data=1,1,1,1,1,-1,0,1,-1,1,1,-1,1,0,-1,-1,1,1,0,1,1,1,1,-1,-1,0,-1,1,1,1'
 response = requests.get(API_LINK)
 status = response.status_code
-print(status)
 if status == 200:
     print(response.json())

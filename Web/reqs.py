e6639b92b1cddf34a2b54f526d6457634367ea67
Or Zellinger <or.zellinger@gmail.com>
2025-07-28 09:07:28 +0300

    Remove Reqs File

--- Web/reqs.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-API_LINK = 'http://127.0.0.1:8000/check?data=1,1,1,1,1,-1,0,1,-1,1,1,-1,1,0,-1,-1,1,1,0,1,1,1,1,-1,-1,0,-1,1,1,1'
-response = requests.get(API_LINK)
-status = response.status_code
-if status == 200:
-    print(response.json())
